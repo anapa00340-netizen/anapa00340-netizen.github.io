9422c2cd8f3f31fdff9ea3139933df81bbed5d1a
anapa00340-netizen <anapa00340@gmail.com>
2026-09-01 15:25:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('#tecnologias', { duration: 1300 });
+
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('#sobre', { duration: 1300 });
+
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('#projetos', { duration: 1300 });
+
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.contato-conteudo', { duration: 1300 });
